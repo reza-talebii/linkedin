45eccede391882774521b7409618794b5570184a
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-20 15:22:22 +0430

    nav styled

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import styled from "styled-components";
 import logoImage from "../../images/login-logo.svg";
 import classes from "./Login.module.css";
 
 const Login = () => {
   return (
     <section className={classes.container}>
-      <nav>
+      <nav className={classes.nav}>
         <a href="/">
           <img src={logoImage} alt="logo" />
         </a>
@@ -14,12 +13,5 @@ const Login = () => {
     </section>
   );
 };
-
-// Styles
-
-// const Container = styled.section`
-//   max-width:
-// `;
-// const Nav = styled.nav``;
 
 export default Login;
