1399bfdc103339364287c064574bb68499fdc3a9
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-21 14:26:04 +0430

    nav list styled

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 import classes from "./Navbar.module.css";
 
+import navHomeImg from "../../images/nav-home.svg";
+
 const Navbar = () => {
-  return <nav className={classes.nav}>Navbar</nav>;
+  return (
+    <nav className={classes.nav}>
+      <ul className={classes.nav__wrap}>
+        <li className={classes.nav__list}>
+          <a>
+            <img src={navHomeImg} />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
