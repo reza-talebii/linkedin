1e44c300657c10412607ac117412cfb0931919c5
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-21 12:22:45 +0430

    logo home image

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import homeLogo from "../../../images/home-logo.svg";
 
 import classes from "./Home.module.css";
 
 const Home = () => {
   return (
     <section className={classes.container}>
-      <section className={classes.content}>Home</section>
+      <section className={classes.content}>
+        <span className={classes.logo}>
+          <Link to="/">
+            <img src={homeLogo} alt="logo" />
+          </Link>
+        </span>
+      </section>
     </section>
   );
 };
