5c2cf75951029ddeecfdc268c5c798da76ebcd75
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-21 16:18:46 +0430

    title home page styled

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,19 @@ import React from "react";
 import classes from "./Home.module.css";
 
 const Home = () => {
-  return <h1>home</h1>;
+  return (
+    <section className={classes.container}>
+      <section className={classes.content}>
+        <h5>
+          <a>Hiring in a hurry? - </a>
+        </h5>
+        <p>
+          Find talented pros in record time with Upwork and keep business
+          moving.
+        </p>
+      </section>
+    </section>
+  );
 };
 
 export default Home;
