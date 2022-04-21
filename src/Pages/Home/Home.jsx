c1a2c2f9877cd06451c98efe8aabb517bf24fa67
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-21 16:21:39 +0430

    Update Home.jsx

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import classes from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <section className={classes.container}>
+      <section className={classes.content}>
+        <h5>
+          <a>Hiring in a hurry? - </a>
+        </h5>
+        <p>
+          Find talented pros in record time with Upwork and keep business
+          moving.
+        </p>
+      </section>
+
+      <div className={classes.layout}></div>
+    </section>
+  );
+};
+
+export default Home;
