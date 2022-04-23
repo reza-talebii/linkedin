31a9d2d78c7e13fb4d791a5111c61abb1c23fe54
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-23 14:58:13 +0430

    create store

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from "./App";
 import "./index.css";
 
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import Store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={Store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
