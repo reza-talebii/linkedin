f03726e4a4d154b4ac899dd21251fda2be774d6a
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-23 11:35:29 +0430

    complete tags sections

--- src/Components/Layout/Tags/Tags.jsx
+++ src/Components/Layout/Tags/Tags.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import classes from "./Tags.module.scss";
+import classes from "./Tags.module.css";
 
 import { feedIcon, rightIcon, seeJobsIcon } from "../../../images";
 
