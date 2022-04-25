22a91b8d7970e8a38aaaf09cf68255ed2a0ea3a0
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-25 14:33:03 +0430

    create post components

---
+++ src/Components/Layout/Posts/Post.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+import {
+  userIcon,
+  ellipsisIcon,
+  likeIcon,
+  commentIcon,
+  sharedImageIcon,
+  sendIcon,
+} from "../../../images";
+
+const Post = ({ post }) => {
+  return (
+    <section className={classes.article}>
+      <div className={classes.shareActor}>
+        <a>
+          <img src={userIcon} alt="avatar" />
+          <div>
+            <span>Title</span>
+            <span>Info</span>
+            <span>Date</span>
+          </div>
+        </a>
+        <button>
+          <img src={ellipsisIcon} alt="" />
+        </button>
+      </div>
+
+      <div className={classes.description}>description</div>
+
+      <div className={classes.sharedImg}>
+        <a>
+          <img src={sharedImageIcon} alt="" />
+        </a>
+      </div>
+
+      <ul className={classes.socialCounts}>
+        <li>
+          <button>
+            <img
+              src="https://static-exp1.licdn.com/sc/h/8ekq8gho1ruaf8i7f86vd1ftt"
+              alt=""
+            />
+            <img
+              src="https://static-exp1.licdn.com/sc/h/cm8d2ytayynyhw5ieaare0tl3"
+              alt=""
+            />
+            <span>75</span>
+          </button>
+        </li>
+        <li>
+          <a> comments</a>
+        </li>
+      </ul>
+
+      <ul className={classes.socialActions}>
+        <button>
+          <img src={likeIcon} alt="" />
+          <span>Like</span>
+        </button>
+        <button>
+          <img src={commentIcon} alt="" />
+          <span>Comment</span>
+        </button>
+        <button>
+          <img src={sharedImageIcon} alt="" />
+          <span>Share</span>
+        </button>
+        <button>
+          <img src={sendIcon} alt="" />
+          <span>Send</span>
+        </button>
+      </ul>
+    </section>
+  );
+};
+
+export default Post;
