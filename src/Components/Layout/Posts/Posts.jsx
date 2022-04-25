d97f5464a7cddccba9e9ef3b3497467e2c39f5bc
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-25 15:08:02 +0430

    complete post

--- src/Components/Layout/Posts/Posts.jsx
+++ src/Components/Layout/Posts/Posts.jsx
@@ -1,93 +1,28 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import classes from "./Posts.module.css";
 
 import { getAllPost } from "../../../store/Posts/PostsThunk";
 
-import {
-  userIcon,
-  ellipsisIcon,
-  likeIcon,
-  commentIcon,
-  sharedImageIcon,
-  sendIcon,
-} from "../../../images";
-
-import SharePost from "./SharePost";
+import { SharePost, Post } from "../../";
 
 const Posts = () => {
+  const user = useSelector((state) => state?.auth?.user);
+  const posts = useSelector((state) => state?.posts?.posts);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getAllPost());
+    if (user) dispatch(getAllPost());
   }, [dispatch]);
 
   return (
     <section className={classes.container}>
       <SharePost />
 
-      <section className={classes.article}>
-        <div className={classes.shareActor}>
-          <a>
-            <img src={userIcon} alt="avatar" />
-            <div>
-              <span>Title</span>
-              <span>Info</span>
-              <span>Date</span>
-            </div>
-          </a>
-          <button>
-            <img src={ellipsisIcon} alt="" />
-          </button>
-        </div>
-
-        <div className={classes.description}>description</div>
-
-        <div className={classes.sharedImg}>
-          <a>
-            <img src={sharedImageIcon} alt="" />
-          </a>
-        </div>
-
-        <ul className={classes.socialCounts}>
-          <li>
-            <button>
-              <img
-                src="https://static-exp1.licdn.com/sc/h/8ekq8gho1ruaf8i7f86vd1ftt"
-                alt=""
-              />
-              <img
-                src="https://static-exp1.licdn.com/sc/h/cm8d2ytayynyhw5ieaare0tl3"
-                alt=""
-              />
-              <span>75</span>
-            </button>
-          </li>
-          <li>
-            <a> comments</a>
-          </li>
-        </ul>
-
-        <ul className={classes.socialActions}>
-          <button>
-            <img src={likeIcon} alt="" />
-            <span>Like</span>
-          </button>
-          <button>
-            <img src={commentIcon} alt="" />
-            <span>Comment</span>
-          </button>
-          <button>
-            <img src={sharedImageIcon} alt="" />
-            <span>Share</span>
-          </button>
-          <button>
-            <img src={sendIcon} alt="" />
-            <span>Send</span>
-          </button>
-        </ul>
-      </section>
+      {posts?.map((post, i) => (
+        <Post key={i} post={post} />
+      ))}
     </section>
   );
 };
